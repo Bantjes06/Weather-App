349f47e331d152196ffa96d99b4010df0b785c15
Johan Bantjes <82716250+Bantjes06@users.noreply.github.com>
2022-07-05 17:33:58 +0200

    All functionality added and project is now completed

--- app.js
+++ app.js
@@ -1,15 +1,28 @@
-const weather = new Weather('London', 'UK');
+const sessionManager = new SessionManager();
+const weatherData = sessionManager.getLocationData();
+const weather = new Weather(weatherData.city, weatherData.state);
+const ui = new UI();
 
-//Event Listeners
+// Event Listeners
 document.addEventListener('DOMContentLoaded', getWeather);
+document.getElementById('w-change-btn').addEventListener('click', e => {
+    const city = document.getElementById('city').value;
+    const state = document.getElementById('state').value;
+
+    weather.changeWeather(city, state);
+
+    getWeather();
+
+    $('#locModal').modal('hide');
+})
 
 function getWeather() {
     weather.getWeather()
         .then(weather => {
             if (Boolean(weather)) {
-                console.log(weather);
+                ui.setWeather(weather);
             } else {
-                document.getElementById('error-message').innerContent = 'City or Province Not Found';
+                document.getElementById('error-message').textContent = 'City or State Not Found';
 
                 setTimeout(() => document.getElementById('error-message').textContent = '', 2500);
             }
--- weather.js
+++ weather.js
@@ -1,26 +1,31 @@
 class Weather {
-    constructor(city, province) {
+
+    constructor(city, state) {
         this.apiKey = '351524ce6ad92e365af96adbd0761cfd';
         this.city = city;
-        this.province = province;
+        this.state = state;
     }
 
     async getWeather() {
-        const result = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${this.city},${this.province}&appid=${this.apiKey}`)
+        const result = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${this.city},${this.state}&appid=${this.apiKey}`);
 
         const responseData = await result.json();
 
         if (responseData.cod === '404') return null;
 
-        responseData.location = { city: this.city, province: this.province};
+        responseData.location = { city: this.city, state: this.state };
 
         return responseData;
     }
 
-    changeWeather(city, province) {
-        if (city === '' || province === '') return;
+    changeWeather(city, state) {
+        if (city === '' || state === '') return;
 
         this.city = city;
-        this.province = province;
+        this.state = state;
+
+        const sessionManager = new SessionManager();
+
+        sessionManager.setLocationData(this.city, this.state);
     }
 }
